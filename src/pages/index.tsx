c116fed922f7af661bc6b0e3879048ea4c303a41
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-07-10 18:14:50 +0700

    feat: removed Hero to component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,9 @@
-import Image from "next/image";
+import Hero from "../components/Main/Hero"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="mask-container relative h-fit">
-        <div className="color-image bg-[url('/Zaki2023.png')] absolute w-full h-full bg-cover inset-0"></div>
-        <img
-          src="/Zaki2023-bw.png"
-          alt="BnW Image"
-          // width={300}
-          // height={400}
-          className="w-[300px]"
-        />
-      </div>
+    <main className="min-h-screen">
+      <Hero />
     </main>
   );
 }
