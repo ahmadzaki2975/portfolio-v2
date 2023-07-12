78836cd468a0516efbb1d04314e8e402bc3ea82c
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-07-12 19:53:54 +0700

    feat: preparations for AboutMe section

---
+++ src/components/Main/AboutMe.tsx
@@ -0,0 +1,7 @@
+export default function AboutMe() {
+  return(
+    <section className="font-poppins min-h-screen flex flex-col justify-center items-center">
+      <h2 className="text-[35px] font-semibold">About Me</h2>
+    </section>
+  )
+}
