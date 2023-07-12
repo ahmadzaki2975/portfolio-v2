04ca7a87f82f7f91614f78f1c5e75f0cbd9bade6
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-07-12 19:54:20 +0700

    feat: render AboutMe in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Main/Hero"
+import AboutMe from "../components/Main/AboutMe"
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Hero />
+      <AboutMe />
     </main>
   );
 }
