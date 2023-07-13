1f013363f6135ebec8dd14accc2ee9a58090eaf8
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-07-13 20:06:34 +0700

    feat: add temporary content

--- src/components/Main/AboutMe.tsx
+++ src/components/Main/AboutMe.tsx
@@ -1,7 +1,12 @@
 export default function AboutMe() {
-  return(
+  return (
     <section className="font-poppins min-h-screen flex flex-col justify-center items-center">
       <h2 className="text-[35px] font-semibold">About Me</h2>
+      <p className="w-1/2">
+        Hello there! My name is Zaki, I'm currently an Information Engineering
+        student in Universitas Gadjah Mada. I'm a passionate Web Developer, and trying to 
+        expand my reach in other fields within the IT industry.
+      </p>
     </section>
-  )
+  );
 }
