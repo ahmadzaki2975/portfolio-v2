7c0209eaa76fe7956557a6be5e1b89d430fd8cec
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-07-14 20:15:10 +0700

    feat: create Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,29 @@
+export default function Footer() {
+  return (
+    <footer className="font-poppins outline">
+      <section className="flex justify-between px-20 py-12">
+        <img src="/Z-Logo-2 1.png" alt="Logo" className="w-[80px] h-[80px]" />
+
+        <div className="flex gap-10">
+          <div>
+            <h3 className="font-semibold text-[21px]">Contact Me</h3>
+            <p className="text-[17px]">lorem ipsum</p>
+            <p className="text-[17px]">lorem ipsum</p>
+          </div>
+          <div>
+            <h3 className="font-semibold text-[21px]">Medias</h3>
+            <p className="text-[17px]">lorem ipsum</p>
+            <p className="text-[17px]">lorem ipsum</p>
+          </div>
+          <div>
+            <h3 className="font-semibold text-[21px]">Navigation</h3>
+            <p className="text-[17px]">lorem ipsum</p>
+            <p className="text-[17px]">lorem ipsum</p>
+          </div>
+        </div>
+      </section>
+
+      <section></section>
+    </footer>
+  );
+}
